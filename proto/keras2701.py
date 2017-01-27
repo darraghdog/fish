db7b7b3c54ee3da9c2653e3c0ba96734f50c0c46
darraghdog <darragh.hanley@gmail.com>
2017-01-27 20:30:30 +0000

    Set up structure

---
+++ proto/keras2701.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 27 20:21:05 2017
+
+@author: darragh
+"""
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+
+from subprocess import check_output
+#print(check_output(["ls", "../input"]).decode("utf8"))
+
+# Any results you write to the current directory are saved as output.
+
+import numpy as np
+np.random.seed(1984)
+
+import os
+import glob
+import cv2
+import datetime
+import pandas as pd
+import time
+import warnings
+warnings.filterwarnings("ignore")
+
+from sklearn.cross_validation import KFold
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D, AveragePooling2D
+from keras.optimizers import SGD, Adagrad
+from keras.callbacks import EarlyStopping
+from keras.utils import np_utils
+from keras.constraints import maxnorm
+from sklearn.metrics import log_loss
+from keras import __version__ as keras_version
+
+def get_im_cv2(path):
+    img = cv2.imread(path)
+    resized = cv2.resize(img, (64, 64), cv2.INTER_LINEAR)
+    return resized
+
+
+def load_train():
+    X_train = []
+    X_train_id = []
+    y_train = []
+    start_time = time.time()
+
+    print('Read train images')
+    folders = ['ALB', 'BET', 'DOL', 'LAG', 'NoF', 'OTHER', 'SHARK', 'YFT']
+    for fld in folders:
+        index = folders.index(fld)
+        print('Load folder {} (Index: {})'.format(fld, index))
+        path = os.path.join('..', 'input', 'train', fld, '*.jpg')
+        files = glob.glob(path)
+        for fl in files:
+            flbase = os.path.basename(fl)
+            img = get_im_cv2(fl)
+            X_train.append(img)
+            X_train_id.append(flbase)
+            y_train.append(index)
+
+    print('Read train data time: {} seconds'.format(round(time.time() - start_time, 2)))
+    return X_train, y_train, X_train_id
+
+
+def load_test():
+    path = os.path.join('..', 'input', 'test_stg1', '*.jpg')
+    files = sorted(glob.glob(path))
+
+    X_test = []
+    X_test_id = []
+    for fl in files:
+        flbase = os.path.basename(fl)
+        img = get_im_cv2(fl)
+        X_test.append(img)
+        X_test_id.append(flbase)
+
+    return X_test, X_test_id
+
+
+def create_submission(predictions, test_id, info):
+    result1 = pd.DataFrame(predictions, columns=['ALB', 'BET', 'DOL', 'LAG', 'NoF', 'OTHER', 'SHARK', 'YFT'])
+    result1.loc[:, 'image'] = pd.Series(test_id, index=result1.index)
+    now = datetime.datetime.now()
+    sub_file = 'submission_' + info + '_' + str(now.strftime("%Y-%m-%d-%H-%M")) + '.csv'
+    result1.to_csv(sub_file, index=False)
