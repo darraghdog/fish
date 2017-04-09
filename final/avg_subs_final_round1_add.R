f2a0963a4c47c3e85ed5a6e9da137f644bf6804d
darraghdog <darragh.hanley@gmail.com>
2017-04-09 13:46:26 +0100

    vgg pseudo sub

--- final/avg_subs_final_round1_add.R
+++ final/avg_subs_final_round1_add.R
@@ -3,7 +3,7 @@ rm(list=ls())
 gc();gc()
 
 # Set working directory 
-setwd("../")
+setwd("~/fish/")
 
 # Load up the sub files 
 subm_01   <- fread("sub/subm_full_conv_anno_1.csv")
@@ -14,11 +14,6 @@ subm_03C  <- fread("sub/subm_full_conv_resnet_3C.csv")
 subm_04A  <- fread("sub/subm_full_convsq_resnet_4A.csv")
 subm_04B  <- fread("sub/subm_full_convsq_resnet_4B.csv")
 subm_04C  <- fread("sub/subm_full_convsq_resnet_4C.csv")
-subm_06   <- fread("sub/subm_full_conv_pseudo_6.csv")
-subm_07A  <- fread("sub/subm_full_convsq_resnet_7A.csv")
-subm_07B  <- fread("sub/subm_full_convsq_resnet_7B.csv")
-subm_07C  <- fread("sub/subm_full_convsq_resnet_7C.csv")
-# rfcn <- fread("~/Dropbox/fish/sub/rfcn.csv")[order(image)]
 
 # Load up the yolo bounding boxes
 yolo <- data.table(read.table("yolo_coords/comp4_det_test_FISH544.txt", quote="\"", comment.char=""))
@@ -43,63 +38,29 @@ setnames(subm_04, "image_file", "image")
 subm_04 = subm_04[order(image)]
 rm(subm_04A, subm_04B, subm_04C)
 
-# Get the average of the box cropped - yolo 414
-subm_07 = subm_07A
-cols = names(subm_07)[-1]
-for (var in cols) subm_07[[var]] = (subm_07A[[var]] + subm_07B[[var]] + subm_07C[[var]])/3
-setnames(subm_07, "image_file", "image")
-subm_07 = subm_07[order(image)]
-rm(subm_07A, subm_07B, subm_07C)
-
-
-###############################
-##  Merge partial data set ####
-###############################
-
-# Get the weighted average of the 414 predictions
-subm_034 = subm_03
-cols = names(subm_034)[-1]
-for (var in cols) subm_034[[var]] = (subm_03[[var]]*.5) + (subm_04[[var]]*.5) 
-subm_034 = subm_034[order(image)]
-
-# Get the outersect of the 414 and 544 images
-img_intrsct = intersect(subm_07$image, subm_034$image)
-subm_part01 = subm_07[!image %in% img_intrsct]
-subm_part02 = subm_034[!image %in% img_intrsct]
-
-# Get the weighted average of the 544 and 414 predictions
-subm_part03 = subm_034[image %in% img_intrsct]
-for (var in cols) subm_part03[[var]] = (subm_034[image %in% img_intrsct][[var]]*.5) + (subm_07[image %in% img_intrsct][[var]]*.5) 
-
-# Add all the parts together
-subm_part = rbind(subm_part01, subm_part02, subm_part03)
-subm_part = subm_part[order(image)]
-rm(subm_part01, subm_part02, subm_part03)
 
 ###############################
 ####  Full data set ###########
 ###############################
 subm_01 = subm_01[order(image)]
 subm_02 = subm_02[order(image)]
-subm_06 = subm_06[order(image)]
-
 
 # Get the weighted average
 subm_012 = subm_01
 cols = names(subm_012)[-1]
-for (var in cols) subm_012[[var]] = ((subm_01[[var]]*.25) + (subm_02[[var]]*.25)  + (subm_06[[var]]*.5))# *.7 +  rfcn[[var]]*.3
+for (var in cols) subm_012[[var]] = (subm_01[[var]]*.5) + (subm_02[[var]]*.5) 
 subm_012 = subm_012[order(image)]
-rm(subm_01, subm_02, subm_06)
+
 
 ###############################
 ##### Merge both ##############
 ###############################
 
 subm_final = subm_012
-id = subm_part$image
+id = subm_03$image
 cols = names(subm_final)[-1]
 for (var in cols) subm_final[image %in% id][[var]] = (subm_012[image %in% id][[var]]*0.3 +    # Original Preds
-                                                        (subm_part[image %in% id][[var]])*0.7)  # Yolo 414 and 544 Preds
+                                                        (subm_03[image %in% id][[var]]*0.4 + subm_04[image %in% id][[var]]*0.6)*0.7)  # Yolo 414 Preds
 
 ###############################
 ##### Thresholding ############
@@ -114,16 +75,13 @@ subm_final = data.table(subm_final)
 # Boost the scores of the very seldom classes.
 id = grep("image", subm_final$image)
 subm_final[id]$image = paste0("test_stg2/",subm_final[id]$image)
-subm_final[LAG>0.4][["LAG"]] = 10
-subm_final[OTHER>0.4][["OTHER"]] = 10
+subm_final[LAG>0.5][["LAG"]] = 10
+subm_final[OTHER>0.5][["OTHER"]] = 10
 subm_final = data.frame(subm_final)
 subm_final[,2:9] = subm_final[,2:9]/rowSums(data.frame(subm_final[,2:9]))
 subm_final = data.table(subm_final)
 
 # Round #1 Sub
-write.csv(subm_final, paste0("sub/final-add-544yolo-addVGGpseudo.csv"), row.names = F)
+write.csv(subm_final, paste0("sub/final-round1-input-pseudo-add_stg2.csv"), row.names = F)
 
-
-# Apr 1st - Adding pseu VGG : 0.493 from 0.508 
-# Apr 1st - Adding yolo 544 : 0.508 from 0.528 
 
